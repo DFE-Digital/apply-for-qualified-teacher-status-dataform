e550a869989b681c8b0c42af67346485bd4e42f1
Noelyn Onah <google-oauth2|116449526953293969688>
2022-11-10 12:45:20 +0000

    changed status to state and created geo and spoke tables

--- definitions/dfe_analytics_dataform.js
+++ definitions/dfe_analytics_dataform.js
@@ -58,7 +58,7 @@ dfeAnalyticsDataform({
       dataType: "string",
       description: "",
     }, {
-      keyName: "status",
+      keyName: "state",
       dataType: "string",
       description: "",
     }, {
