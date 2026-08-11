566b5e481111e24987a277df760ecb9ba4c64fdb
alex.christensen@education.gov.uk <alex.christensen@education.gov.uk>
2026-08-11 11:51:50 +0000

    Replaced Applications_customquery with a key table for afqts

---
+++ includes/countries.js
@@ -0,0 +1,17 @@
+function mapping(countryField) {
+    return `
+CASE
+  WHEN LOWER(${countryField}) IN (
+    'austria','belgium','bulgaria','croatia','cyprus','czech republic',
+    'denmark','estonia','finland','france','germany','greece','hungary',
+    'ireland','italy','latvia','lithuania','luxembourg','malta',
+    'netherlands','poland','portugal','romania','slovakia','slovenia',
+    'spain','sweden') THEN 'eu'
+  WHEN LOWER(${countryField}) IN ('iceland','liechtenstein','norway') THEN 'eea'
+  WHEN LOWER(${countryField}) IN ('united states','australia','new zealand','gibraltar','scotland','canada') THEN 'ott'
+END
+`;
+}
+module.exports = {
+    mapping
+};
